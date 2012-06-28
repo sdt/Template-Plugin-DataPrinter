13a59f444434fae77d30ab374b95d4674ede1102
Stephen Thirlwall <sdt@dr.com>
2012-06-28 17:16:36 +1000

    Clear import list for HTML::FromANSI::Tiny

--- lib/Template/Plugin/DataPrinter.pm
+++ lib/Template/Plugin/DataPrinter.pm
@@ -6,7 +6,7 @@ use base 'Template::Plugin';
 # ABSTRACT: Template Toolkit dumper plugin for Data::Printer
 # VERSION
 
-use HTML::FromANSI::Tiny;
+use HTML::FromANSI::Tiny ();
 use Hash::Merge::Simple qw< merge >;
 
 sub new {
